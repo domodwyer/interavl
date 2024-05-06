b179d884566ae588c754b42d0fc154d181237fa6
Dom Dwyer <dom@itsallbroken.com>
2024-05-06 23:16:16 +0200

    feat: entry deletion
    Allow nodes and their values to be deleted from the tree.

--- src/avl.rs
+++ src/avl.rs
@@ -1,7 +1,11 @@
-use crate::node::Node;
+use std::cmp::Ordering;
+
+use crate::node::{remove_recurse, Node, RemoveResult};
 
 #[derive(Debug, Default)]
 pub(crate) struct IntervalTree(Option<Box<Node>>);
+
+// TODO(dom): iter, range iter
 
 impl IntervalTree {
     pub(crate) fn insert(&mut self, value: usize) {
@@ -17,10 +21,19 @@ impl IntervalTree {
             .map(|v| v.contains(value))
             .unwrap_or_default()
     }
+
+    pub(crate) fn remove(&mut self, value: usize) -> Option<usize> {
+        match remove_recurse(&mut self.0, value)? {
+            RemoveResult::Removed(v) => Some(v),
+            RemoveResult::ParentUnlink => unreachable!(),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashSet;
+
     use proptest::prelude::*;
 
     use super::*;
@@ -41,9 +54,85 @@ mod tests {
         assert!(!t.contains(26));
         assert!(!t.contains(43));
         assert!(!t.contains(41));
+
+        validate_tree_structure(&t);
     }
 
-    const N_VALUES: usize = 50;
+    #[test]
+    fn test_insert_remove_contains() {
+        const N: usize = 50;
+
+        let mut t = IntervalTree::default();
+
+        for i in 0..N {
+            t.insert(i);
+        }
+
+        for i in 0..(N / 2) {
+            t.remove(i);
+        }
+
+        for i in 0..(N / 2) {
+            assert!(!t.contains(i));
+        }
+
+        for i in (N / 2)..N {
+            assert!(t.contains(i));
+        }
+
+        validate_tree_structure(&t);
+    }
+
+    #[test]
+    fn test_update_after_subtree_min_extract() {
+        let values = [
+            28, 44, 49, 16, 2, 29, 30, 7, 48, 46, 26, 31, 11, 21, 6, 19, 33, 42, 36, 0, 41, 23, 43,
+            5, 14,
+        ];
+
+        let mut t = IntervalTree::default();
+
+        // Insert all the values.
+        for &v in &values {
+            t.insert(v);
+        }
+
+        validate_tree_structure(&t);
+
+        // Ensure contains() returns true for all of them and remove all
+        // values that were inserted.
+        for &v in &values {
+            // Remove the node (that should exist).
+            assert!(t.contains(v));
+            assert_eq!(t.remove(v), Some(v));
+
+            // Attempting to remove the value a second time is a no-op.
+            assert!(!t.contains(v));
+            assert_eq!(t.remove(v), None);
+
+            // At all times, the tree must be structurally sound.
+            validate_tree_structure(&t);
+        }
+    }
+
+    const N_VALUES: usize = 200;
+
+    #[derive(Debug)]
+    enum Op {
+        Insert(usize),
+        Contains(usize),
+        Remove(usize),
+    }
+
+    fn arbitrary_op() -> impl Strategy<Value = Op> {
+        // A small value domain encourages multiple operations to act on the
+        // same value.
+        prop_oneof![
+            (0..15_usize).prop_map(Op::Insert),
+            (0..15_usize).prop_map(Op::Contains),
+            (0..15_usize).prop_map(Op::Remove),
+        ]
+    }
 
     proptest! {
         /// Insert values into the tree and assert contains() returns true for
@@ -75,53 +164,140 @@ mod tests {
             for &v in b.difference(&a) {
                 assert!(!t.contains(v));
             }
+
+            validate_tree_structure(&t);
         }
 
-        /// Assert the BST and AVL properties of tree nodes, ensuring the tree
-        /// is well-formed.
+        /// Insert values into the tree and delete them after, asserting they
+        /// are removed and the extracted values are returned.
         #[test]
-        fn prop_node_invariants(
-            values in prop::collection::vec(any::<usize>(), 1..N_VALUES),
+        fn prop_insert_contains_remove(
+            values in prop::collection::hash_set(0..N_VALUES, 0..N_VALUES),
         ) {
             let mut t = IntervalTree::default();
-            for v in values {
+
+            // Insert all the values.
+            for &v in &values {
                 t.insert(v);
             }
 
-            // Perform a pre-order traversal of the tree.
-            let mut stack = vec![t.0.as_deref().unwrap()];
-            while let Some(n) = stack.pop() {
-                // Prepare to visit the children
-                stack.extend(n.left().iter().chain(n.right().iter()));
+            validate_tree_structure(&t);
 
-                // Invariant 1: the left child always contains a value strictly
-                // less than this node.
-                assert!(n.left().map(|v| v.value() < n.value()).unwrap_or(true));
+            // Ensure contains() returns true for all of them and remove all
+            // values that were inserted.
+            for &v in &values {
+                // Remove the node (that should exist).
+                assert!(t.contains(v));
+                assert_eq!(t.remove(v), Some(v));
 
-                // Invariant 2: the right child always contains a value strictly
-                // greater than this node.
-                assert!(n.right().map(|v| v.value() > n.value()).unwrap_or(true));
+                // Attempting to remove the value a second time is a no-op.
+                assert!(!t.contains(v));
+                assert_eq!(t.remove(v), None);
 
-                // Invariant 3: the height of this node is always +1 of the
-                // maximum child height.
-                let left_height = n.left().map(|v| v.height());
-                let right_height = n.right().map(|v| v.height());
-                let want_height = left_height
-                    .max(right_height)
-                    .map(|v| v + 1) // This node is +1 of the child, if any
-                    .unwrap_or_default(); // Otherwise it is at height 0
-
-                assert_eq!(n.height(), want_height);
-
-                // Invariant 4: the absolute height difference between the left
-                // subtree and right subtree (the "balance factor") cannot
-                // exceed 1.
-                let balance = left_height
-                    .and_then(|l| right_height.map(|r| l as i64 - r as i64))
-                    .unwrap_or_default()
-                    .abs();
-                assert!(balance <= 1, "balance={balance}");
+                // At all times, the tree must be structurally sound.
+                validate_tree_structure(&t);
             }
+
+            assert_eq!(t.remove(N_VALUES+1), None);
+        }
+
+        #[test]
+        fn prop_tree_operations(
+            ops in prop::collection::vec(arbitrary_op(), 1..50),
+        ) {
+            let mut t = IntervalTree::default();
+            let mut model = HashSet::new();
+
+            for op in ops {
+                match op {
+                    Op::Insert(v) => {
+                        // TODO(dom): return bool for insert
+                        t.insert(v);
+                        model.insert(v);
+                    },
+                    Op::Contains(v) => {
+                        assert_eq!(
+                            t.contains(v),
+                            model.contains(&v),
+                            "tree contains() = {}, model.contains() = {}",
+                            t.contains(v),
+                            model.contains(&v)
+                        );
+                    },
+                    Op::Remove(v) => {
+                        let t_got = t.remove(v);
+                        let model_got = model.remove(&v);
+                        assert_eq!(
+                            t_got.is_some(),
+                            model_got,
+                            "tree remove() = {:?}, model.remove() = {}",
+                            t_got,
+                            model_got,
+                        );
+                    },
+                }
+
+                // At all times, the tree must uphold the AVL tree invariants.
+                validate_tree_structure(&t);
+            }
+
+            for v in model {
+                assert!(t.contains(v));
+            }
+        }
+    }
+
+    /// Assert the BST and AVL properties of tree nodes, ensuring the tree
+    /// is well-formed.
+    fn validate_tree_structure(t: &IntervalTree) {
+        let root = match t.0.as_deref() {
+            Some(v) => v,
+            None => return,
+        };
+
+        // Perform a pre-order traversal of the tree.
+        let mut stack = vec![root];
+        while let Some(n) = stack.pop() {
+            // Prepare to visit the children
+            stack.extend(n.left().iter().chain(n.right().iter()));
+
+            // Invariant 1: the left child always contains a value strictly
+            // less than this node.
+            assert!(n.left().map(|v| v.value() < n.value()).unwrap_or(true));
+
+            // Invariant 2: the right child always contains a value striggctly
+            // greater than this node.
+            assert!(n.right().map(|v| v.value() > n.value()).unwrap_or(true));
+
+            // Invariant 3: the height of this node is always +1 of the
+            // maximum child height.
+            let left_height = n.left().map(|v| v.height());
+            let right_height = n.right().map(|v| v.height());
+            let want_height = left_height
+                .max(right_height)
+                .map(|v| v + 1) // This node is +1 of the child, if any
+                .unwrap_or_default(); // Otherwise it is at height 0
+
+            assert_eq!(
+                n.height(),
+                want_height,
+                "expect node with value {} to have height {}, has {}",
+                n.value(),
+                want_height,
+                n.height(),
+            );
+
+            // Invariant 4: the absolute height difference between the left
+            // subtree and right subtree (the "balance factor") cannot
+            // exceed 1.
+            let balance = left_height
+                .and_then(|l| right_height.map(|r| l as i64 - r as i64))
+                .unwrap_or_default()
+                .abs();
+            assert!(
+                balance <= 1,
+                "balance={balance}, node={n:?}, stack={stack:?}"
+            );
         }
     }
 }
--- src/node.rs
+++ src/node.rs
@@ -1,5 +1,15 @@
 use std::cmp::Ordering;
 
+#[derive(Debug)]
+pub(super) enum RemoveResult<T> {
+    /// The value was removed from the tree.
+    Removed(T),
+
+    /// The direct descendent node contains the value, but contains no children
+    /// and must be unlinked by the parent.
+    ParentUnlink,
+}
+
 #[derive(Debug, Clone)]
 pub(crate) struct Node {
     value: usize,
@@ -26,19 +36,19 @@ impl Node {
     }
 
     pub(crate) fn insert(self: &mut Box<Self>, value: usize) {
-        let node = match value.cmp(&self.value) {
+        let child = match value.cmp(&self.value) {
             Ordering::Less => &mut self.left,
             Ordering::Equal => return,
             Ordering::Greater => &mut self.right,
         };
 
-        match node {
+        match child {
             Some(v) => {
                 v.insert(value);
             }
             None => {
                 // Insert the value as a new immediate descendent of self.
-                *node = Some(Box::new(Self::new(value)));
+                *child = Some(Box::new(Self::new(value)));
 
                 // Inserting this new child node cannot skew the tree in the
                 // direction of the new addition such that it requires the tree
@@ -61,16 +71,18 @@ impl Node {
         //
         // If the balance factor is now 2/-2, then the above insertion has added
         // to an already 1/-1 skewed subtree - therefore the subtree the value
-        // was applied to can be used to infer which subtree is now unbalanced
-        // as a result.
+        // was applied to can be used to infer which child subtree is now
+        // unbalanced as a result.
         match (balance(self), self.left.as_ref(), self.right.as_ref()) {
+            // Left-heavy
             (2, Some(l), _) if value < l.value => {
-                *self = rotate_right(self.clone());
+                *self = rotate_right(self.clone()); // TODO(dom): clone is recursive!
             }
             (2, Some(l), _) if value > l.value => {
                 self.left = Some(rotate_left(self.left.take().unwrap()));
                 *self = rotate_right(self.clone());
             }
+            // Right-heavy
             (-2, _, Some(r)) if value > r.value => {
                 *self = rotate_left(self.clone());
             }
@@ -78,12 +90,119 @@ impl Node {
                 self.right = Some(rotate_right(self.right.take().unwrap()));
                 *self = rotate_left(self.clone());
             }
-            _ => { /* The tree is well balanced */ }
+            (-1..=1, _, _) => { /* The tree is well balanced */ }
+            _ => unreachable!(),
         };
 
         // Invariant: the absolute difference between tree heights ("balance
         // factor") cannot exceed 1.
         debug_assert!(balance(self).abs() <= 1);
+    }
+
+    pub(super) fn remove(self: &mut Box<Self>, value: usize) -> Option<RemoveResult<usize>> {
+        // Recurse down the subtree rooted at `self`.
+        //
+        // If the value is not found, or successfully removed, the result is
+        // returned. If the direct descendent node contains the value and no
+        // children, it returns [`RemoveResult::ParentUnlink`] and the node is
+        // unlinked here in the parent before returning the result to the
+        // caller.
+        match value.cmp(&self.value()) {
+            Ordering::Less => return remove_recurse(&mut self.left, value),
+            Ordering::Greater => return remove_recurse(&mut self.right, value),
+            Ordering::Equal => {
+                // This node holds the value to be removed from the tree.
+                debug_assert_eq!(self.value, value);
+            }
+        };
+
+        // This node may have 0, 1 or 2 child node(s):
+        //
+        //                          +----------+
+        //                          |  parent  |
+        //                          +----------+
+        //                                |
+        //                                v
+        //                          +----------+
+        //                     +----|   self   |----+
+        //                     |    +----------+    |
+        //                     |                    |
+        //                     v                    v
+        //               +-----------+       +------------+
+        //               | self.left |       | self.right |
+        //               +-----------+       +------------+
+        //
+        // The minimum successor child (if any) should move to replace this
+        // node.
+        //
+        // If the "self.right" has a left child, descend the left-most edge to
+        // locate the successor to "self" returned in an in-order traversal and
+        // use it in place of "self". The right child of "self" after removing
+        // this successor (if any) is then linked to this replacement.
+        //
+        // If there is no left node of "self.right", the "self.right" itself
+        // replaces the target node (the minimum subtree successor value).
+        //
+        // If there is no right child, then "self.left" replaces "self".
+        let old = if let Some(mut right) = self.right.take() {
+            debug_assert_ne!(self.height, 0);
+
+            // Extract the minimum node in the node_right subtree, if any.
+            match extract_subtree_min(&mut right) {
+                Some(mut min) => {
+                    // This minimum node "min" should be mutated to link
+                    // self.right on the right, and self.left (if any) linked on
+                    // the left in order to preserve the binary search property.
+                    //
+                    // The "min" node is guaranteed to have no left pointer as
+                    // it is the left-most / minimum node in the subtree.
+                    debug_assert!(min.left.is_none());
+                    debug_assert!(min.right.is_none());
+
+                    min.left = self.left.take();
+                    min.right = Some(right);
+
+                    std::mem::replace(self, min)
+                }
+
+                None => {
+                    // Otherwise the extracted "right" is the successor, and can
+                    // replace "self".
+                    //
+                    // It is guaranteed that "right" has no left pointer,
+                    // otherwise the above branch would be taken.
+                    debug_assert!(right.left.is_none());
+
+                    right.left = self.left.take();
+                    std::mem::replace(self, right)
+                }
+            }
+        } else if let Some(left) = self.left.take() {
+            // Otherwise, if "self" has a left child only, simply replace "self"
+            // with the left child (the minimum subtree value).
+            debug_assert!(self.right.is_none());
+            debug_assert_ne!(self.height, 0);
+
+            std::mem::replace(self, left)
+        } else {
+            // Otherwise "self" has no children.
+            debug_assert!(self.left.is_none());
+            debug_assert!(self.right.is_none());
+            debug_assert_eq!(self.height, 0);
+
+            // Parent will unlink this "self" node.
+            return Some(RemoveResult::ParentUnlink);
+        };
+
+        // Invariant: the node being unlinked contains no subtree.
+        debug_assert!(old.right.is_none());
+        debug_assert!(old.left.is_none());
+
+        // Invariant: the old node being unlinked does contain the target value.
+        debug_assert_eq!(old.value, value);
+        debug_assert_ne!(self.value, value); // The replacement node does not.
+
+        Some(RemoveResult::Removed(old.value))
     }
 
     pub(crate) fn contains(&self, value: usize) -> bool {
@@ -108,8 +227,16 @@ impl Node {
         self.left.as_deref()
     }
 
+    pub(crate) fn left_mut(&mut self) -> Option<&mut Box<Self>> {
+        self.left.as_mut()
+    }
+
     pub(crate) fn right(&self) -> Option<&Self> {
         self.right.as_deref()
+    }
+
+    pub(crate) fn right_mut(&mut self) -> Option<&mut Box<Self>> {
+        self.right.as_mut()
     }
 }
 
@@ -126,9 +253,13 @@ fn update_height(n: &mut Node) {
         .unwrap_or_default()
 }
 
+/// Compute the "balance factor" of the subtree rooted at `n`.
+///
+/// Returns the subtree height skew / magnitude, which is a positive number when
+/// left heavy, and a negative number when right heavy.
 fn balance(n: &Node) -> i8 {
-    // Correctness: the height is a u8, the maximal value of which fits in a i16
-    // without truncation or sign inversion.
+    // Correctness: the height is a u8, the maximal value of which fits in an
+    // i16 without truncation or sign inversion.
     (height(n.left()) as i16 - height(n.right()) as i16) as i8
 }
 
@@ -185,6 +316,100 @@ fn rotate_right(mut y: Box<Node>) -> Box<Node> {
     update_height(&mut p);
 
     p
+}
+
+/// Extracts the node holding the minimum subtree value in a descendent of
+/// `root`, if any, linking the right subtree of the extracted node to in its
+/// place.
+fn extract_subtree_min(root: &mut Box<Node>) -> Option<Box<Node>> {
+    // Descend left to the leaf.
+    let v = match extract_subtree_min(root.left.as_mut()?) {
+        Some(mut v) => Some(v),
+        None => {
+            // The left child is the end of the left edge.
+            //
+            // ```text
+            //                 6
+            //                / \
+            //    here ->   <4>   7
+            //              / \
+            //             2   5
+            //              \
+            //               3
+            // ```
+            //
+            // Unlink the right node of the left root, which will become the new
+            // left node of "root" (if any).
+            let mut left_right = root.left.as_mut().and_then(|v| v.right.take());
+
+            std::mem::replace(&mut root.left, left_right)
+        }
+    };
+
+    rebalance_after_remove(root);
+    debug_assert!(balance(root).abs() <= 1);
+    v
+}
+
+/// Recurse into `node`, calling [`Node::remove()`] to remove the provided
+/// `value` from the subtree rooted at `node`, if it exists.
+///
+/// Returns [`None`] if the value is not found.
+///
+/// Clears the `node` pointer if the [`Node::remove()`] call returns
+/// [`RemoveResult::ParentUnlink`], returning the extracted value within a
+/// [`RemoveResult::Removed`] variant.
+pub(super) fn remove_recurse(
+    node: &mut Option<Box<Node>>,
+    value: usize,
+) -> Option<RemoveResult<usize>> {
+    // Remove the value (if any) and rebalance the tree.
+    let remove_ret = node.as_mut().and_then(|v| {
+        let ret = v.remove(value)?;
+        rebalance_after_remove(v);
+        Some(ret)
+    })?;
+
+    let v = match remove_ret {
+        RemoveResult::Removed(v) => v,
+        RemoveResult::ParentUnlink => node.take().unwrap().value,
+    };
+
+    debug_assert_eq!(v, value);
+
+    Some(RemoveResult::Removed(v))
+}
+
+fn rebalance_after_remove(v: &mut Box<Node>) {
+    // Recompute the height of the relocated node.
+    update_height(v);
+
+    // And rebalance the subtree.
+    match (balance(v)) {
+        (2..) if v.left.as_deref().map(balance).unwrap_or_default() >= 0 => {
+            *v = rotate_right(v.clone()); // TODO(dom): subtree clone
+        }
+        (2..) if v.left.as_deref().map(balance).unwrap_or_default() < 0 => {
+            v.left = v.left.take().map(rotate_left);
+            *v = rotate_right(v.clone()); // TODO(dom): subtree clone
+        }
+        (..=-2) if v.right.as_deref().map(balance).unwrap_or_default() <= 0 => {
+            *v = rotate_left(v.clone()); // TODO(dom): subtree clone
+        }
+        (..=-2) if v.right.as_deref().map(balance).unwrap_or_default() > 0 => {
+            v.right = v.right.take().map(rotate_right);
+            *v = rotate_left(v.clone()); // TODO(dom): subtree clone
+        }
+
+        #[allow(clippy::manual_range_patterns)]
+        -1 | 0 | 1 => { /* balanced */ }
+
+        _ => unreachable!(),
+    }
+
+    // Invariant: the absolute difference between tree heights ("balance
+    // factor") cannot exceed 1 after removing a value.
+    debug_assert!(balance(v).abs() <= 1);
 }
 
 #[cfg(test)]
@@ -295,5 +520,42 @@ mod tests {
             let right = right_root.right().unwrap();
             assert_eq!(right.value, 7);
         }
+    }
+
+    #[test]
+    fn test_extract_subtree_min() {
+        //
+        //          6
+        //         / \
+        //        4   7
+        //       / \
+        //      2   5
+        //     / \
+        //    1   3
+        //
+        let mut t = Box::new(Node::new(6));
+        add_right(&mut t, 7);
+        let v = add_left(&mut t, 4);
+        add_right(v, 5);
+        let v = add_left(v, 2);
+        add_right(v, 3);
+        add_left(v, 1);
+
+        for want in [1, 2, 3] {
+            let n: Box<Node> = extract_subtree_min(&mut t).unwrap();
+            assert_eq!(n.value, want);
+            assert!(n.right.is_none());
+        }
+
+        assert!(extract_subtree_min(&mut t).is_none());
+        assert!(extract_subtree_min(&mut t).is_none());
+
+        assert!(t.left.is_none());
+        assert_eq!(t.value, 4);
+
+        let right = t.right.as_ref().unwrap();
+        assert_eq!(right.value, 6);
+        assert_eq!(right.left.as_ref().unwrap().value, 5);
+        assert_eq!(right.right.as_ref().unwrap().value, 7);
     }
 }
